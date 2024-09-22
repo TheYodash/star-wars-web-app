8293c8cdb8442866c0094f55aa06552d5d675a09
Moayad Mohammed <moayad.ashabi@gmail.com>
2024-09-23 01:38:24 +0200

    edited copywrite text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
                 <a href="https://www.linkedin.com/in/moashabi/" target="_blank" rel="noreferrer"><FaLinkedin className='footer-icon' /> LinkedIn</a>
                 <a href="https://github.com/TheYodash" target="_blank" rel="noreferrer"><FaGithubSquare className='footer-icon'/> GitHub</a>
             </div>
-            <p>Star Wars and all associated are copyright of Lucasfilm</p>
+            <p>Star Wars and all associated items are copyright of Lucasfilm</p>
                 <p>DESIGNED AND DEVELOPED BY <strong>MOAYAD MOHAMMED</strong></p>
         </footer>
     );
